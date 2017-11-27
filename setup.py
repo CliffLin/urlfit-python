13309debf159f9dbccc93c65de960b237a511f1d
Ze-Yan Lin <zylin@cs.nctu.edu.tw>
2017-11-27 14:07:52 +0800

    correct package name

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 
 setup(
 	name='UrlFit',
-	version='1.0.2',
+	version='1.0.3',
 	description='UrlFit - Shorten your url',
 	author='Ze-Yan Lin',
 	author_email='zylin@url.fit',
 	url='https://url.fit',
 	install_requires = ['requests[security]','cryptography'],
-	packages=['src'],
+	packages=['urlfit'],
 	entry_points={"console_scripts": ["urlfit=src.urlfit:main"]}
-     )
+)
 
